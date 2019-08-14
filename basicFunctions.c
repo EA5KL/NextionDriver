81851f4a738caf1502f9797603d06d64e36f9317
ElGamal <706513+ElGamal@users.noreply.github.com>
2019-08-14 18:10:27 +0200

    Update basicFunctions.c

--- basicFunctions.c
+++ basicFunctions.c
@@ -40,7 +40,7 @@
       if (mode == C_DMR_XMODE) {
 	 if (modeIsEnabled[C_DMR]) {
 	    if ( (proc_find("MMDVMHost")) && (proc_find("DMR2YSF") || proc_find("DMR2NXDN")) ) {
-		   sprintf(text, "msg.txt=\"%d\"", proc_find("DMR2YSF"));
+		   sprintf(text, "msg.txt=\"%d-%d-%d\"", proc_find("MMDVMHost"), proc_find("DMR2YSF"), proc_find("DMR2NXDN"));
 		   sendCommand(text);
 		   return(1);
 	    }
