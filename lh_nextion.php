990178bec86fb833b6ba6338e32651d5da7a1a79
ElGamal <706513+ElGamal@users.noreply.github.com>
2019-08-16 13:51:55 +0200

    Update lh_nextion.php

--- lh_nextion.php
+++ lh_nextion.php
@@ -4,30 +4,33 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDa
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php';    // MMDVMDash Functions
 
 $i = 0;
-for ($i = 0;  ($i <= 14); $i++) { //Last 15 calls
+$LHlist = "";
+for ($i = 0;  ($i <= 14); $i++) { // Last 15 calls
         if (isset($lastHeard[$i])) {
                 $listElem = $lastHeard[$i];
-                echo substr(str_pad($listElem[1], 11), 0, 11)."|";
-                echo substr(str_pad($listElem[2], 10), 0, 11)."|";
-                echo substr(str_pad($listElem[4], 9), 0 , 9)."|";
+                $LHlist .= "t".strval($i+101).'.txt="';
+                $LHlist .= substr(str_pad($listElem[1], 11), 0, 11)."|";
+                $LHlist .= substr(str_pad($listElem[2], 11), 0, 11)."|";
+                $LHlist .= substr(str_pad($listElem[4], 9), 0 , 9)."|";
 
                 if ($listElem[5] == "RF") {
-                        echo "RF |";
+                        $LHlist .= "RF |";
                 } else {
-                        echo str_pad($listElem[5], 3)."|";
+                        $LHlist .= str_pad($listElem[5], 3)."|";
                 }
 
                 if ($listElem[6] == null) {
-                        echo "TX   |";
+                        $LHlist .= "TX   |";
                 } else if ($listElem[6] == "SMS") {
-                        echo "SMS  |";
+                        $LHlist .= "SMS  |";
                 } else {
-                        echo str_pad($listElem[6], 5, " ", STR_PAD_LEFT)."|";
+                        $LHlist .= str_pad($listElem[6], 5, " ", STR_PAD_LEFT)."|";
                 }
 
-                echo str_pad($listElem[7], 3," ", STR_PAD_LEFT)."|";
-                echo str_pad($listElem[8], 4," ", STR_PAD_LEFT)."|";
-                echo"</br>\n";
+                $LHlist .= str_pad($listElem[7], 3," ", STR_PAD_LEFT)."|";
+                $LHlist .= str_pad($listElem[8], 4," ", STR_PAD_LEFT)."|";
+                $LHlist .= '"';
         }
 }
+echo $LHlist;
 ?>
