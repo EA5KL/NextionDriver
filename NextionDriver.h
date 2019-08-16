8ac217d860b1e19cec4b9599861eaa80ffecc752
ElGamal <706513+ElGamal@users.noreply.github.com>
2019-08-16 19:11:35 +0200

    Update NextionDriver.h

--- NextionDriver.h
+++ NextionDriver.h
@@ -19,7 +19,7 @@
 #if !defined(NextionDriver_H)
 #define NextionDriver_H
 
-#define NextionDriver_VERSION "1.247"
+#define NextionDriver_VERSION "1.248"
 
 /* the code to extract RX and TX freq and location has been
     included in MMDVMHost 20180910 and later, so
