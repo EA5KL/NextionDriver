8c6242875465992e0bf3bd9104c9d338aa7055f6
ElGamal <706513+ElGamal@users.noreply.github.com>
2019-08-21 11:58:20 +0200

    Added code for LastHeard list

--- processButtons.c
+++ processButtons.c
@@ -49,10 +49,7 @@ void sendLHlist() {
       curl_easy_perform(curl);
       curl_easy_cleanup(curl);
     }
-
- // Sustituir la cadena constante por la llamada al modulo lh_nextion.php	
- // sprintf(text, "DMR Slot 2 |EB5EGK     |TG 214   |Net| 21.0| 0%|0.0%DMR Slot 1 |TA3NHI     |TG 91    |Net|  0.5| 0%|0.0%DMR Slot 2 |EA5GLZ     |TG 214   |Net| 41.5| 0%|0.0%DMR Slot 1 |9V1MH      |TG 91    |Net|  0.4| 0%|0.0%DMR Slot 1 |DA6RAM     |TG 91    |Net|  0.8| 0%|0.0%DMR Slot 2 |EA5HFB     |TG 214   |Net|  0.8| 0%|0.0%DMR Slot 2 |EA4LO      |TG 214   |Net|  0.5| 0%|0.0%DMR Slot 1 |DW1HBF     |TG 91    |Net|  0.5| 0%|0.0%DMR Slot 2 |EB4ERW     |TG 214   |Net|  0.5| 0%|0.0%DMR Slot 2 |EA9SV      |TG 214   |Net|  0.5| 0%|0.0%DMR Slot 1 |EA3GMP     |TG 91, 5 |Net|TX   |   |    POCSAG     |DAPNET     |DAPNET Us|Net|  0.0| 0%|0.0%DMR Slot 2 |EA5GK      |TG 214   |Net|  0.1| 0%|0.0%DMR Slot 2 |EB4AXR     |TG 214   |Net|  0.5| 0%|0.0%DMR Slot 1 |M0AUT      |TG 91    |Net|  0.8| 0%|0.0%");
-	for (i=1; i<=15; i++) {
+    for (i=1; i<=15; i++) {
 	c = 0;
 	while (c < length) { 
             exit[c] = lhreceived[(length * (i - 1)) + c];
@@ -63,13 +60,11 @@ void sendLHlist() {
     }
 	
 //	if (res != 0) {
-//           sendCommand(text);
 //           sendCommand("MMDVM.status.val=98");
 //	} else {
 //           sendCommand("LHt1.txt=\"No data in last heard list\"");
 //           sendCommand("MMDVM.status.val=99");
-//        } 
-//}
+//      } 
 }
 
 
