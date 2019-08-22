26c7cfc161597a16f651dfdf6de6a5e0a4b471ab
ElGamal <706513+ElGamal@users.noreply.github.com>
2019-08-22 14:42:49 +0200

    Start adding support for local last heard list

--- lh_nextion.php
+++ lh_nextion.php
@@ -3,32 +3,41 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/config/config.php';          // MMDVMDa
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/tools.php';        // MMDVMDash Tools
 include_once $_SERVER['DOCUMENT_ROOT'].'/mmdvmhost/functions.php';    // MMDVMDash Functions
 
+
 $i = 0;
-$LHlist = "";
+$LHlistNET = "";
+$LHlistLOC = "";
 for ($i = 0;  ($i <= 14); $i++) { // Last 15 calls
         if (isset($lastHeard[$i])) {
                 $listElem = $lastHeard[$i];
-                $LHlist .= substr(str_pad($listElem[1], 11), 0, 11)."|";
-                $LHlist .= substr(str_pad($listElem[2], 11), 0, 11)."|";
-                $LHlist .= substr(str_pad($listElem[4], 9), 0 , 9)."|";
+                $LHlistNET .= substr(str_pad($listElem[1], 11), 0, 11)."|";
+                $LHlistNET .= substr(str_pad($listElem[2], 11), 0, 11)."|";
+                $LHlistNET .= substr(str_pad($listElem[4], 9), 0 , 9)."|";
 
                 if ($listElem[5] == "RF") {
-                        $LHlist .= "RF |";
+                        $LHlistNET .= "RF |";
                 } else {
-                        $LHlist .= str_pad($listElem[5], 3)."|";
+                        $LHlistNET .= str_pad($listElem[5], 3)."|";
                 }
 
                 if ($listElem[6] == null) {
-                        $LHlist .= "TX   |";
+                        $LHlistNET .= "TX   |";
                 } else if ($listElem[6] == "SMS") {
-                        $LHlist .= "SMS  |";
+                        $LHlistNET .= "SMS  |";
                 } else {
-                        $LHlist .= str_pad($listElem[6], 5, " ", STR_PAD_LEFT)."|";
+                        $LHlistNET .= str_pad($listElem[6], 5, " ", STR_PAD_LEFT)."|";
                 }
 
-                $LHlist .= str_pad($listElem[7], 3," ", STR_PAD_LEFT)."|";
-                $LHlist .= str_pad($listElem[8], 4," ", STR_PAD_LEFT);
+                $LHlistNET .= str_pad($listElem[7], 3," ", STR_PAD_LEFT)."|";
+                $LHlistNET .= str_pad($listElem[8], 4," ", STR_PAD_LEFT);
         }
 }
-echo $LHlist;
+if (isset($_GET["from"])) {
+   if ($_GET["from"] == "NET")
+      echo $LHlistNET;
+   else if ($_GET["from"] == "LOC")
+      echo $LHlistLOC;
+} else {
+   echo "There is no last heard data"; // Fallback behaviour goes here
+}
 ?>
