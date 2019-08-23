44e24db3952d8ef95359b58f81039c774b1627f5
ElGamal <706513+ElGamal@users.noreply.github.com>
2019-08-23 10:18:58 +0200

    Update processButtons.c

--- processButtons.c
+++ processButtons.c
@@ -38,7 +38,7 @@ void function_pt(void *ptr, size_t size, size_t nmemb, void *stream){
 
 void sendLHlist(unsigned char param) {
 
-    char buf[52], exit[52], LHlistURL[74]
+    char buf[52], exit[52], LHlistURL[74];
     int c, i, length = 52; // 52 is the length of the LHt(x) fields in nextion screen
 
     if (param == 0x01)
